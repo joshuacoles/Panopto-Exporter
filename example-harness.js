01d57e2ba26584f922b95271929c348ed1a611ac
Joshua Coles <josh@coles.to>
2021-05-28 10:40:21 +0100

    Example harness

---
+++ example-harness.js
@@ -0,0 +1,13 @@
+require('./src').run({
+    baseUrl: 'URL',
+    credentials: {
+        username: 'username',
+        password: 'password'
+    },
+    copyrightAttribution: 'something'
+}, [
+    {
+        targetID: 'algebra',
+        folderID: '1065facb-4f61-40d1-9cce-b43f8adeece3',
+    }
+])
